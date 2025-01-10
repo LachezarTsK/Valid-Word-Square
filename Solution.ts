9860f8d4dcd2afd8931f562d84f85c011e5682b8
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-10 21:50:55 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,15 @@
+
+function validWordSquare(words: string[]): boolean {
+    const numberOfWords = words.length;
+    for (let row = 0; row < numberOfWords; ++row) {
+        if (words[row].length > numberOfWords) {
+            return false;
+        }
+        for (let column = 0; column < words[row].length; ++column) {
+            if (words[column].length - 1 < row || words[column][row] !== words[row][column]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
